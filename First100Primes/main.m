0447b7bd3a8f783316e515b6a78f71e3530eb0ce
Stephanie L <sunlove_systems@yahoo.com>
2014-04-26 08:06:43 -0400

    Initial Commit

---
+++ First100Primes/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  First100Primes
+//
+//  Created by Transcend on 4/26/14.
+//  Copyright (c) 2014 SunLoveSystems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
